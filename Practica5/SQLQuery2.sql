4f288b59cfb68f89e7e292e44ef53933d605d02c
lesliecarreon17 <36417131+lesliecarreon17@users.noreply.github.com>
2018-03-17 09:56:10 -0600

    Add files via upload

---
+++ Practica5/SQLQuery2.sql
@@ -0,0 +1,111 @@
+use netflix
+GO
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Guillermo del Toro'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Pablo Larrin'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Richard Linklater'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Carlos Vermut'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Paolo Sorrentino'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Harmoni Korine'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Joachim Lafosse'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Xavier Dolan'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Leos Carax'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Carlos Reygadas'
+	)
+
+
+delete 
+from Autores
+where IdAutores = '12345899'
+
+select * from Autores
+
+Insert into Subscripcion(
+		nombre, costo
+	)
+
+values
+	(
+		
+		
+	 
+
+
