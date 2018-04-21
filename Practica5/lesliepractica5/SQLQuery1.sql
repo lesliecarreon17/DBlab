930fd32b45e2bcf9393eeca0d56f6a795445e721
lesliecarreon17 <36417131+lesliecarreon17@users.noreply.github.com>
2018-04-21 10:06:45 -0500

    Add files via upload

---
+++ Practica5/lesliepractica5/SQLQuery1.sql
@@ -0,0 +1,5 @@
+select *
+from Autores
+
+select *
+from compras
---
+++ Practica5/lesliepractica5/SQLQuery6.sqleslie.sql
@@ -0,0 +1,589 @@
+use netflix
+go
+
+Insert into Director(
+	IdDirector, nombre
+	)
+
+values
+	(
+	  1547896,'Steven Spielberg'
+	)
+
+
+Insert into Director(
+	IdDirector, nombre
+	)
+
+values
+	(
+	   1547897, 'Peter jackson'
+	)
+
+
+Insert into Director(
+	IdDirector, nombre
+	)
+
+values
+	(
+	   1547898,'Martin Scorsese'
+	)
+
+Insert into Director(
+	IdDirector, nombre
+	)
+
+values
+	(
+	   1547899, 'Christopher Nolan'
+	)
+
+Insert into Director(
+	IdDirector, nombre
+	)
+
+values
+	(
+	   1547900, 'Steven Soderbergh'
+	)
+
+Insert into Director(
+	IdDirector, nombre
+	)
+
+values
+	(
+	   1547901, 'Ridley Scott'
+	)
+
+Insert into Director(
+	IdDirector, nombre
+	)
+
+values
+	(
+	   1547902, 'Quentin Tarantino'
+	)
+
+Insert into Director(
+	IdDirector, nombre
+	)
+
+values
+	(
+	   1547903, 'Michael Mann'
+	)
+
+Insert into Director(
+	IdDirector, nombre
+	)
+
+values
+	(
+	   1547904, 'James Cameron'
+	)
+
+Insert into Director(
+	IdDirector, nombre
+	)
+
+values
+	(
+	   1547905, 'Guillermo del Toro'
+	)
+
+select * from Director
+
+
+Insert into Genero(
+	IdGenero, tipo
+	)
+
+values
+	(
+	  1758960,'Drama'
+	)
+
+Insert into Genero(
+	IdGenero, tipo
+	)
+
+values
+	(
+	   1758961, 'Comedia'
+	)
+
+
+Insert into Genero(
+	IdGenero, tipo
+	)
+
+values
+	(
+	  1758962, 'Accion'
+	)
+
+
+
+Insert into Genero(
+	IdGenero, tipo
+	)
+
+values
+	(
+	  1758963, 'Ciencia Ficcion'
+	)
+
+
+Insert into Genero(
+	IdGenero, tipo
+	)
+
+values
+	(
+	  1758964, 'Fantasia'
+	)
+
+
+Insert into Genero(
+	IdGenero, tipo
+	)
+
+values
+	(
+	  1758965, 'Terror'
+	)
+
+
+Insert into Genero(
+	IdGenero, tipo
+	)
+
+values
+	(
+	  1758966, 'Romance'
+	)
+
+
+Insert into Genero(
+	IdGenero, tipo
+	)
+
+values
+	(
+	  1758967, 'Musical'
+	)
+
+
+
+Insert into Genero(
+	IdGenero, tipo
+	)
+
+values
+	(
+	  1758968, 'Melodrama'
+	)
+
+
+Insert into Genero(
+	IdGenero, tipo
+	)
+
+values
+	(
+	  1758969, 'Suspenso'
+	)
+
+select * from Genero
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Guillermo del Toro'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Pablo Larrin'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Richard Linklater'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Carlos Vermut'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Paolo Sorrentino'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Harmoni Korine'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Joachim Lafosse'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Xavier Dolan'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Leos Carax'
+	)
+
+Insert into Autores(
+	 Nombre
+	)
+
+values
+	(
+	  'Carlos Reygadas'
+	)
+
+
+delete 
+from Autores
+where IdAutores = '12345898'
+
+select * from Autores
+
+
+Insert into Usuario(
+	 IdUsuario, Nombre, telefono
+	)
+
+values
+	(
+	 2589643, 'Elena Griego', 81865932
+	)
+
+Insert into Usuario(
+	 IdUsuario, Nombre, telefono
+	)
+
+values
+	(
+	 2589644, 'Elisa Hebreo', 81865859
+	)
+
+Insert into Usuario(
+	 IdUsuario, Nombre, telefono
+	)
+
+values
+	(
+	 2589641, 'Eloisa Germano', 81865264
+	)
+
+Insert into Usuario(
+	 IdUsuario, Nombre, telefono
+	)
+
+values
+	(
+	 2589642, 'Eloy Frances', 81865019
+	)
+
+Insert into Usuario(
+	 IdUsuario, Nombre, telefono
+	)
+
+values
+	(
+	 2589645, 'Baldomero Germano', 81865364
+	)
+
+
+Insert into Usuario(
+	 IdUsuario, Nombre, telefono
+	)
+
+values
+	(
+	 2589646, 'Belen Hebrero', 81865927
+	)
+
+Insert into Usuario(
+	 IdUsuario, Nombre, telefono
+	)
+
+values
+	(
+	 2589647, 'Benigno Latino', 81865483
+	)
+
+Insert into Usuario(
+	 IdUsuario, Nombre, telefono
+	)
+
+values
+	(
+	 2589648, 'Benjamin Hebrero', 81865083
+	)
+
+Insert into Usuario(
+	 IdUsuario, Nombre, telefono
+	)
+
+values
+	(
+	 2589649, 'Berenice Griego', 81865980
+	)
+
+Insert into Usuario(
+	 IdUsuario, Nombre, telefono
+	)
+
+values
+	(
+	 2589650, 'Leslie Carreon', 8771098445
+	)
+
+select * from Usuario
+
+
+
+Insert into Subscripcion(
+	 IdSubscripcion, Nombre, costo
+	)
+
+values
+	(
+	 358961, 'Infantil', 100
+	)
+Insert into Subscripcion(
+	 IdSubscripcion, Nombre, costo
+	)
+
+values
+	(
+	 358962, 'Familiar', 300
+	)
+
+Insert into Subscripcion(
+	 IdSubscripcion, Nombre, costo
+	)
+
+values
+	(
+	 358963, 'Infantil', 100
+	)
+
+Insert into Subscripcion(
+	 IdSubscripcion, Nombre, costo
+	)
+
+values
+	(
+	 358964, 'Familiar', 300
+	)
+
+Insert into Subscripcion(
+	 IdSubscripcion, Nombre, costo
+	)
+
+values
+	(
+	 358965, 'Individual', 500
+	)
+
+Insert into Subscripcion(
+	 IdSubscripcion, Nombre, costo
+	)
+
+values
+	(
+	 358966, 'Infantil', 100
+	)
+Insert into Subscripcion(
+	 IdSubscripcion, Nombre, costo
+	)
+
+values
+	(
+	 358967, 'Individual', 500
+	)
+
+Insert into Subscripcion(
+	 IdSubscripcion, Nombre, costo
+	)
+
+values
+	(
+	 358968, 'Familiar', 300
+	)
+
+Insert into Subscripcion(
+	 IdSubscripcion, Nombre, costo
+	)
+
+values
+	(
+	 358969, 'Infantil', 100
+	)
+
+
+Insert into Subscripcion(
+	 IdSubscripcion, Nombre, costo
+	)
+
+values
+	(
+	 3589470, 'Individual', 500
+	)
+
+
+select * from Subscripcion
+
+Insert into MetodoDePago(
+	 IdMetodoDePago, FormaDePago
+	)
+
+values
+	(
+	 4589623, 'tarjeta de regalo'
+	)
+
+Insert into MetodoDePago(
+	 IdMetodoDePago, FormaDePago
+	)
+
+values
+	(
+	 4589620, 'tarjeta de credito'
+	)
+
+Insert into MetodoDePago(
+	 IdMetodoDePago, FormaDePago
+	)
+
+values
+	(
+	 4589621, 'PayPal'
+	)
+
+Insert into MetodoDePago(
+	 IdMetodoDePago, FormaDePago
+	)
+
+values
+	(
+	 4589622, 'tarjeta de prepago'
+	)
+
+Insert into MetodoDePago(
+	 IdMetodoDePago, FormaDePago
+	)
+
+values
+	(
+	 4589624, 'PayPal'
+	)
+
+Insert into MetodoDePago(
+	 IdMetodoDePago, FormaDePago
+	)
+
+values
+	(
+	 4589625, 'tarjeta de credito'
+	)
+
+Insert into MetodoDePago(
+	 IdMetodoDePago, FormaDePago
+	)
+
+values
+	(
+	 4589626, 'PayPal'
+	)
+
+Insert into MetodoDePago(
+	 IdMetodoDePago, FormaDePago
+	)
+
+values
+	(
+	 4589627, 'tarjeta de regalo'
+	)
+
+Insert into MetodoDePago(
+	 IdMetodoDePago, FormaDePago
+	)
+
+values
+	(
+	 4589628, 'tarjeta de prepago'
+	)
+
+Insert into MetodoDePago(
+	 IdMetodoDePago, FormaDePago
+	)
+
+values
+	(
+	 4589629, 'tarjeta credito'
+	)
+
+Insert into MetodoDePago(
+	 IdMetodoDePago, FormaDePago
+	)
+
+values
+	(
+	 4589630, 'tPayPal'
+	)
+
+delete 
+from MetodoDePago
+where IdMetodoDePago = '4589630'
+
+select * from MetodoDePago
